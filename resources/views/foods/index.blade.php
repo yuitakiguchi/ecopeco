8d3f667a010c7ee4927191be5f85ee075c8418e0
yuitakiguchi <y1x1y@docomo.ne.jp>
2020-09-10 20:28:46 +0900

    feature/#14-product-list

--- resources/views/foods/index.blade.php
+++ resources/views/foods/index.blade.php
@@ -6,12 +6,12 @@
   <button type="submit" class="area-search">検索</button>
   <a href="{{ route('foods.create') }}">新規投稿</a>
   <div class="row mb-5">
+    @foreach ($foods as $food)
     <div class="col-md-4 mb-5">
 	    <div class="card text-center">
 			  <div class="card-header">
 				  〇〇店
 			  </div>
-        @foreach ($foods as $food)
 			  <div class="card-body">
 				  <img src="" alt="商品写真">
 				  <p class="card-text">商品名：{{ $food->name }}</p>
@@ -19,9 +19,11 @@
 				  <p class="card-text">{{ $food->price }}円→{{ $food->discount_price }}円</p>
 				  <p class="card-text">クーポン{{ $food->coupon }}枚</p>
 			  </div>
-        @endforeach
+        
 		  </div>
+      
     </div>
+    @endforeach
   </div>
 </div>
 @endsection
