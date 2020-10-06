de9d9458bd4a10fba1bb6eedef4d92ac675e21b3
yuitakiguchi <y1x1y@docomo.ne.jp>
2020-10-06 16:35:14 +0900

    #96-inquiry-form

--- resources/views/contacts/contact.blade.php
+++ resources/views/contacts/contact.blade.php
@@ -1,56 +1,67 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="page-section" id="contact">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-12 text-center">
-        <h2 class="section-heading text-uppercase">Contact</h2>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-lg-12" id="formWrapper">
-        <form id="contactForm" action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSd6ail7k2oJlRMUApv4aaAEwEKSJAGTZR9A_tLJcIwgriMS4w/formResponse" target="dummyIframe" name="myForm" novalidate="novalidate">
-          <div class="row">
-            <div class="col-md-6">
-              <div class="form-group">
-                <input class="form-control" name="entry.1054758800" id="name" type="text" placeholder="Your Name *" required="required"
-                  data-validation-required-message="Please enter your name.">
-                <p class="help-block text-danger"></p>
-              </div>
-              <div class="form-group">
-                <input class="form-control" name="entry.249037719" id="email" type="email" placeholder="Your Email *" required="required"
-                  data-validation-required-message="Please enter your email address.">
-                <p class="help-block text-danger"></p>
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div class="form-group">
-                <textarea class="form-control" name="entry.249037719" id="message" placeholder="Your Message *" required="required"
-                  data-validation-required-message="Please enter a message."></textarea>
-                <p class="help-block text-danger"></p>
-              </div>
-            </div>
-          
-            
-            <div class="col-lg-12 text-center">
-              <div id="success"></div>
-              <button id="sendMessageButton" class="btn btn-primary btn-xl text-uppercase" type="submit" onclick="sendGform()">Send
-                Message</button>
-            </div>
+<section class="page-section text-center" id="contact">
+    <img class="contact2 d-none d-xl-inline-block" src="image/contact2.png">
+    <img class="contact1 d-none d-xl-inline-block" src="image/contact1.png">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <h1>Contact</h1>
+          <img class="contact-image d-none d-xl-inline-block" src="image/sign-up1.png">
+        {{-- </div> --}}
+      
+        {{-- <div class="col-lg-12" id="formWrapper"> --}}
+          <div class="card-body">
+            <form id="contactForm"
+                action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSd6ail7k2oJlRMUApv4aaAEwEKSJAGTZR9A_tLJcIwgriMS4w/formResponse"
+                target="dummyIframe" name="myForm" novalidate="novalidate">
+                {{-- <div class="row"> --}}
+                    {{-- <div class="col-md-6"> --}}
+                        <div class="form-group row">
+                            <input class="form-control" name="entry.1054758800" id="name" type="text"
+                                placeholder="お名前 *" required="required"
+                                data-validation-required-message="Please enter your name.">
+                            <p class="help-block text-danger"></p>
+                        </div>
+
+                        <div class="form-group row">
+                            <input class="form-control" name="entry.249037719" id="email" type="email"
+                                placeholder="メールアドレス *" required="required"
+                                data-validation-required-message="Please enter your email address.">
+                            <p class="help-block text-danger"></p>
+                        </div>
+
+                    {{-- </div> --}}
+                    {{-- <div class="col-md-6"> --}}
+                        <div class="form-group row">
+                            <textarea class="form-control" name="entry.249037719" id="message"
+                                placeholder="メッセージ *" required="required"
+                                data-validation-required-message="Please enter a message."></textarea>
+                            <p class="help-block text-danger"></p>
+                        </div>
+                    {{-- </div> --}}
+
+
+                    <div class="col-lg-12 text-center">
+                        <div id="success"></div>
+                        <button id="sendMessageButton" class="btn btn-primary btn-xl text-uppercase" type="submit"
+                            onclick="sendGform()">送信</button>
+                    </div>
+                {{-- </div> --}}
+            </form>
           </div>
-        </form>
-        <iframe name="dummyIframe" style="display:none;"></iframe>
+          <iframe name="dummyIframe" style="display:none;"></iframe>
+            <div id="thxMessage" style="display:none;">お問い合わせありがとうございました。</div>
+          <script>
+            function sendGform() {
+                document.myForm.submit();
+                document.getElementById('formWrapper').style.display = 'none';
+                document.getElementById('thxMessage').style.display = 'block';
+            }
+          </script>
+        </div>
       </div>
-      <div id="thxMessage" style="display:none;">お問い合わせありがとうございました。</div>
-      <script>
-        function sendGform(){
-          document.myForm.submit();
-          document.getElementById('formWrapper').style.display = 'none';
-          document.getElementById('thxMessage').style.display = 'block';
-        }
-        </script>
     </div>
-  </div>
 </section>
 @endsection
