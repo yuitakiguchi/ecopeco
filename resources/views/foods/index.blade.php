51050d2cfe5ad0489e4260225d16b6ac2a3ffd61
yuitakiguchi <y1x1y@docomo.ne.jp>
2020-10-10 19:27:00 +0900

    #85-food-index-user-design

--- resources/views/foods/index.blade.php
+++ resources/views/foods/index.blade.php
@@ -2,57 +2,61 @@
 
 @section('content')
 @if (Auth::user()->authority_id === \App\User::AUTHORITY_USER)
-<div class="container">
-  <form action="{{ url('/foods') }}" method="GET">
-    <input type="text" name="keyword" placeholder="地域を検索する" value="{{ $keyword }}" >
-    <button type="submit" class="area-search">検索</button>
-  </form>
-  @if ($keyword)
-    @if($area->count())
-      <p>検索結果</p>
-      <div class="row mb-5 justify-content-center">
-        @foreach ($area->users as $user)
-          @foreach ($user->foods as $food)
-            <div class="col-md-6 col-xl-3 mb-5 text-center mx-auto">
-              <a href="{{ route('foods.show',$food->id) }}">
-                <div class="card">
-                  <img src="{{ $food->image_name }}" class="card-img-top" alt="商品画像">
-                  <div class="card-body">
-                    <h5 class="card-title">商品名：{{ $food->name }}</h5>
-                    <p class="card-text">引取日：{{ $food->trading_date }}</p>
-                    <p class="card-text">引取時間：{{ $food->trading_time }}</p>
-                    <p class="card-text">{{ $food->price }}円→{{ $food->discount_price }}円</p>
-                    <p class="card-text">クーポン{{ $food->coupon }}枚</p>
+<div class="index-food-user">
+  <div class="container">
+    <form action="{{ url('/foods') }}" method="GET">
+      <input class="w-50 p-2" type="text" name="keyword" placeholder="地域を検索する" value="{{ $keyword }}" >
+      <button type="submit" class="area-search btn btn-primary">検索</button>
+    </form>
+    @if ($keyword)
+      @if($area && $area->users())
+        <p class="search-results">検索結果</p>
+        <div class="row mb-5 justify-content-center">
+          @foreach ($area->users as $user)
+            @foreach ($user->foods as $food)
+              <div class="col-md-6 col-xl-3 mb-5 text-center mx-auto">
+                <a href="{{ route('foods.show',$food->id) }}">
+                  <div class="card">
+                    <div class="card-header">{{ $food->user->name }}</div>
+                    <div class="card-body">
+                      <img src="{{ $food->image_name }}" class="card-img-top" alt="商品画像">
+                      <h5 class="card-title">{{ $food->name }}</h5>
+                      <p class="card-text">引取日：{{ $food->trading_date }}</p>
+                      <p class="card-text">引取時間：{{ $food->trading_time }}</p>
+                      <p class="card-text">{{ $food->price }}円→{{ $food->discount_price }}円</p>
+                      <p class="card-text">クーポン{{ $food->coupon }}枚</p>
+                    </div>
                   </div>
-                </div>
-              </a>
-            </div>
+                </a>
+              </div>
+            @endforeach
           @endforeach
+        </div>
+      @else
+        <p>見つかりませんでした。</p>
+      @endif
+    @else
+      <div class="row mb-5 justify-content-center">
+        @foreach ($foods as $food)
+        <div class="col-md-6 col-xl-3 mb-5 text-center mx-auto">
+          <a href="{{ route('foods.show',$food->id) }}">
+            <div class="card">
+              <div class="card-header">{{ $food->user->name }}</div>
+              <div class="card-body">
+                <img src="{{ $food->image_name }}" class="card-img-top" alt="商品画像">
+                <h5 class="card-title">{{ $food->name }}</h5>
+                <p class="card-text">引取日：{{ $food->trading_date }}</p>
+                <p class="card-text">引取時間：{{ $food->trading_time }}</p>
+                <p class="card-text">{{ $food->price }}円→{{ $food->discount_price }}円</p>
+                <p class="card-text">クーポン{{ $food->coupon }}枚</p>
+              </div>
+            </div>
+          </a>
+        </div>
         @endforeach
       </div>
-    @else
-      <p>見つかりませんでした。</p>
     @endif
-  @else
-    <div class="row mb-5 justify-content-center">
-      @foreach ($foods as $food)
-      <div class="col-md-6 col-xl-3 mb-5 text-center mx-auto">
-        <a href="{{ route('foods.show',$food->id) }}">
-          <div class="card">
-            <img src="{{ $food->image_name }}" class="card-img-top" alt="商品画像">
-            <div class="card-body">
-              <h5 class="card-title">商品名：{{ $food->name }}</h5>
-              <p class="card-text">引取日：{{ $food->trading_date }}</p>
-              <p class="card-text">引取時間：{{ $food->trading_time }}</p>
-              <p class="card-text">{{ $food->price }}円→{{ $food->discount_price }}円</p>
-              <p class="card-text">クーポン{{ $food->coupon }}枚</p>
-            </div>
-          </div>
-        </a>
-      </div>
-      @endforeach
-    </div>
-  @endif
+  </div>
 </div>
 @else
 <div class="container">
