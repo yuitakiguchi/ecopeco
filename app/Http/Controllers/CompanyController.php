4690f6f429b7f8427679e07fa0a29dfdf803532e
yuitakiguchi <y1x1y@docomo.ne.jp>
2020-10-15 08:30:50 +0900

    #130-cannot-access-guest

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -12,6 +12,11 @@ use Auth;
 
 class CompanyController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     /**
      * Display a listing of the resource.
      *
