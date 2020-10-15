4d652a333de41c1ce3ca4db46321b17ab29ef5f0
yuitakiguchi <y1x1y@docomo.ne.jp>
2020-10-15 21:22:04 +0900

    heroku

--- app/User.php
+++ app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
     use Notifiable;
 
     const AUTHORITY_COMPANY = 1;
-    const AUTHORITY_USER = 2;
+    const AUTHORITY_USER = 11;
 
     /**
      * The attributes that are mass assignable.
